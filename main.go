f73917140da3070183ea681399beda30b75c3535
ItsCryptic <hello@kevind.me>
2022-04-08 14:05:51 -0400

    i forgot to change the directory of where go gets its files for the web server when...

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	fs := http.FileServer(http.Dir("website"))
+	fs := http.FileServer(http.Dir("static"))
 	fmt.Println("Server started on port 80")
 	log.Fatal(http.ListenAndServe(":80", fs), nil)
 }
